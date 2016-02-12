8a41b1eaaee29cd0f75e8708acd944c255c10614
Simon Ireland <spir109@cse.unsw.edu.au>
2016-02-12 22:54:50 +1100

    Added angular vel calculation: test v1.0 OWRS-112 #comment Added the claculations for angular velocity (directiojn included) derived from the encoders. New logic issues are adjusting if prescaler is required, low speed and the crossover from forward to reverse.

--- encoder.c
+++ encoder.c
@@ -11,12 +11,20 @@
 #include <xc.h>
 
 // Time period measurements for encoders
+unsigned int timePeriod0 = 0;
 unsigned int timePeriod1 = 0;
 unsigned int timePeriod2 = 0;
 unsigned int timePeriod3 = 0;
 unsigned int timePeriod4 = 0;
 unsigned int timePeriod5 = 0;
-unsigned int timePeriod6 = 0;
+
+// Angular speed of the motor
+signed double angVel0 = 0;
+signed double angVel1 = 0;
+signed double angVel2 = 0;
+signed double angVel3 = 0;
+signed double angVel4 = 0;
+signed double angVel5 = 0;
 
 // Timer 5 overlap counters for encoders
 unsigned int enc0 = 0;
@@ -39,9 +47,13 @@ void __attribute__((__interrupt__, no_auto_psv)) _IC1Interrupt(void) {
 
     //TODO: each pulse is 1/3 a rotation, need to account for this in later calculation
     //TODO: if prescaler used, account for that in calculations
-    timePeriod1 = (PR5 * enc0) + t2 - t1; // Calculate time perioid between pulses(part of todo, need to check this with timer period etc)
+    //TODO: account for motor going from +ve to -ve
+    timePeriod0 = (PR5 * enc0) + t2 - t1; // Calculate time perioid between pulses(part of todo, need to check this with timer period etc)
     
     enc0 = 0; //Clear timer overlap counter for encoder 0
+    
+    angVel0 = (-1.0 * PORTBbits.RB9) * ((2 * ENC_PI)/(3 * timePeriod0)); // Calculate Angular velocity of motor. 1st bracket is to test direction.
+                                                                         // Since 3 pulses are sent per rotation, we are measuring a 1/3 rotation.
 }
 
 
@@ -56,6 +68,8 @@ void __attribute__((__interrupt__, no_auto_psv)) _IC2Interrupt(void) {
     timePeriod1 = (PR5 * enc1) + t2 - t1;
     
     enc1 = 0;
+    
+    angVel1 = (-1.0 * PORTBbits.RB11) * ((2 * ENC_PI)/(3 * timePeriod1));
 }
 
 
@@ -67,9 +81,11 @@ void __attribute__((__interrupt__, no_auto_psv)) _IC3Interrupt(void) {
     
     IFS0bits.IC1IF = 0; // Clear interrupt flag
     
-    timePeriod1 = (PR5 * enc2) + t2 - t1;
+    timePeriod2 = (PR5 * enc2) + t2 - t1;
     
     enc2 = 0;
+    
+    angVel2 = (-1.0 * PORTBbits.RB13) * ((2 * ENC_PI)/(3 * timePeriod2));
 }
 
 
@@ -81,9 +97,11 @@ void __attribute__((__interrupt__, no_auto_psv)) _IC4Interrupt(void) {
     
     IFS0bits.IC1IF = 0; // Clear interrupt flag
     
-    timePeriod1 = (PR5 * enc3) + t2 - t1;
+    timePeriod3 = (PR5 * enc3) + t2 - t1;
     
     enc3 = 0;
+    
+    angVel3 = (-1.0 * PORTBbits.RB15) * ((2 * ENC_PI)/(3 * timePeriod3));
 }
 
 
@@ -95,9 +113,11 @@ void __attribute__((__interrupt__, no_auto_psv)) _IC5Interrupt(void) {
     
     IFS0bits.IC1IF = 0; // Clear interrupt flag
     
-    timePeriod1 = (PR5 * enc4) + t2 - t1;
+    timePeriod4 = (PR5 * enc4) + t2 - t1;
     
     enc4 = 0;
+    
+    angVel4 = (-1.0 * PORTFbits.RF5) * ((2 * ENC_PI)/(3 * timePeriod4));
 }
 
 
@@ -110,9 +130,11 @@ void __attribute__((__interrupt__, no_auto_psv)) _IC1Interrupt(void) {
     
     IFS0bits.IC1IF = 0; // Clear interrupt flag
     
-    timePeriod1 = (PR5 * enc5) + t2 - t1;
+    timePeriod5 = (PR5 * enc5) + t2 - t1;
     
     enc5 = 0;
+    
+    angVel5 = (-1.0 * PORTFbits.RF3) * ((2 * ENC_PI)/(3 * timePeriod5));
 }
 
 
