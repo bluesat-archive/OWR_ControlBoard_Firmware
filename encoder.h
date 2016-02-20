0b609e3e2db6945b9b8f3f439d33271bf39f63ac
Simon Ireland <spir109@cse.unsw.edu.au>
2016-02-20 11:40:27 +1100

    added logical directives to encoder.h OWRS-112

--- encoder.h
+++ encoder.h
@@ -1,4 +1,8 @@
 // Header file for encoder interface
+// Created on 13/2/2016
+
+#ifndef ENCODER_H
+#define	ENCODER_H
 
 #define ENC_PI 3.14159265359
 #define TMR_5_PS 64 //Prescaler level
@@ -12,3 +16,5 @@ volatile int16_t angVel2 = 0;
 volatile int16_t angVel3 = 0;
 volatile int16_t angVel4 = 0;
 volatile int16_t angVel5 = 0;
+
+#endif	/* ENCODER_H */
